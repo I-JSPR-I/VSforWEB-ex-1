4f01037252addac53562f0e25fdabcff4ea8ec28
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-05-10 09:04:16 +0200

    Add project files.

---
+++ VSforWEB ex 1/Postback.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Postback.aspx.cs" Inherits="VSforWEB_ex_1.Postback" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+        </div>
+        <p>
+            <asp:Button ID="btnVerstuur" runat="server" OnClick="Button1_Click" Text="Verstuur" Height="113px" Width="113px" />
+
+        </p>
+
+    </form>
+</body>
+</html>
---
+++ VSforWEB ex 1/Postback.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VSforWEB_ex_1
+{
+    public partial class Postback : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack) {
+                Response.Write("Je hebt reeds op VERSTUUR geklikt!");
+            }
+            else {
+                Response.Write("Klik op VERSTUUR om te versturen!");
+            }
+        }
+    }
+}
